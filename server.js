82776cfef4130cd6f9853019627633f683128cf1
mjwash <mjwash@live.unc.edu>
2021-11-13 16:53:39 -0500

    test 1.2

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo").get(req.params.id);
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id

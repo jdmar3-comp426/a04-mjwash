96fe623ec471c2c59766dd09d7c9cb2b154bccc7
mjwash <mjwash@live.unc.edu>
2021-11-13 23:37:31 -0500

    test 1.7

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).json({"message": info.changes+ " record created: ID "+ info.lastInsertRowid});
+	res.status(201).json({"message": info.changes+ " record created: ID "+ info.lastInsertRowid + " (201)"});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -40,9 +40,17 @@ app.get("/app/user/:id", (req, res) => {
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/update/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
+	const info = stmt.run(req.body.user, req.body.pass, req.params.id);
+	res.status(200).json({"message": info.changes+ " record updated: ID "+ info.lastInsertRowid + " (200)"});
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+	const info = stmt.run(req.params.id);
+	res.status(200).json({"message": info.changes+ " record deleted: ID "+ info.lastInsertRowid + " (200)"});
+});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});

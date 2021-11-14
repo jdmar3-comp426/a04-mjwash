c8073caeafbc7a2eaf5fd58eea5bc8b00f1cee53
mjwash <mjwash@live.unc.edu>
2021-11-13 23:42:32 -0500

    test 1.8

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const info = stmt.run(req.body.user, req.body.pass, req.params.id);
+	const info = stmt.run(req.params.id, req.body.user, req.body.pass);
 	res.status(200).json({"message": info.changes+ " record updated: ID "+ info.lastInsertRowid + " (200)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
